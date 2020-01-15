3a1a5cd24a3cf1e0855bb49582010cccd48e5e9c
ned <>
2020-01-15 10:49:01 +0200

    starting to work with NewCourseForm

--- src/views/forms/newcourse.php
+++ src/views/forms/newcourse.php
@@ -1,3 +1,24 @@
 <?php
 
-// smth
+class NewCourseForm {
+    protected $form = [];
+
+    public function construct__() {
+        $this->setForm();
+    }
+
+    public function setForm() {
+        $form = [
+            'fields' => [
+                [
+                    'name' => 'course_name',
+                    'type' => 'text',
+                    'value' => 'Example course',
+                    'placeholder' => 'example text'
+                ],
+            'buttons' => [
+
+            ]
+        ];
+    }
+}
