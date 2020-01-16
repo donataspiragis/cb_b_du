e54804b01747b12227cc8074f1fe479bad93986b
PauliusZiedelis <p.ziedelis@gmail.com>
2020-01-16 11:31:57 +0200

    pcb-47

--- app/controller/LectureController.php
+++ app/controller/LectureController.php
@@ -4,10 +4,39 @@ namespace App\Controller;
 use App\App;
 use App\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Request;
+use App\Model\Model;
 
 class LectureController extends BaseController  {
-    public function index(){
+    public function index($id){
+        //test
+//        return $this->render('forms/videosAdd',['video' => $id]);
     }
+    public function getAllVideosFromChanel(){
+        $channelId = 'UCeTVoczn9NOZA9blls3YgUg';
+        $maxResults = 3;
+//        $API_key = 'AIzaSyC2bzQLTHYtXj7R1TSvlZ-1HyY_OxB1xnI';
+        $API_key = 'AIzaSyD4rNOOOSNEZVG-BBDfexa4kdYl1dE1dgw';
+        $listOfVideoKey= $this->getAllVideosListFromChanel($channelId, $maxResults, $API_key);
+        $max = sizeof( $listOfVideoKey);
+        for($i = 0; $i < $max; $i++) {
+            $videoList[]='https://www.youtube.com/embed/'.$listOfVideoKey[$i];
+        }
+        return $this->render('forms/videosAdd',['lecture' =>$videoList]);
+    }
+    public function  getAllVideosListFromChanel($channelId, $maxResults, $API_key){
+        $video_list = file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelId.'&maxResults='.$maxResults.'&key='.$API_key.'');
+        preg_match_all  ('/("videoId": ")(.+?(?="))/',  $video_list, $matches);
+        $videosList=$matches[2];
+        return $videosList;
+    }
+
+
+
+
+    public function addCursesVideos(){
+
+    }
+
 
 
 }
--- src/views/forms/videosAdd.php
+++ src/views/forms/videosAdd.php
@@ -1,1 +1,11 @@
-<?php
+{% block body %}
+<br><br><br><br>
+<form action="">
+{% for element in lecture %}
+<li><input type="checkbox"> <object width="200" height="100" data="{{element}}"></li>
+</object>
+{% endfor %}
+    <br><br>
+    <input type="submit" value="Submit">
+</form>
+{% endblock %}
