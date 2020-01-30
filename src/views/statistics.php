516014346de71ca9aa25d557a5bbcdc5edbb3f52
Ugnius <ugnius_staniulis@msn.com>
2020-01-30 15:33:47 +0200

    PCB-71
    forgot statistics view

--- src/views/statistics.php
+++ src/views/statistics.php
@@ -1,18 +1,37 @@
 {% extends 'backlayout.php' %}
 {% block title %}Home{% endblock %}
 {% block datacontainer %}
-
+<!--{{ all.price }}
 
 <h1>kkk</h1>
-{% for order in orders %}
-{{ order.invoice_id }}
-{% endfor %}
-<br>
-{% for invoice in invoices %}
-{{ invoice.price }}
-<br>
-{{ invoice.created_on }}
-{% endfor %}
+
+{% for in in all%}
+{{ in.price }}
+{% endfor %}-->
+
+<div class="viewCourse">
+    <div class="statistic">
+        <div class="boxcenter">
+            <h1>Einamas mėnuo <br>{{amount.thisMonth}}</h1>
+        </div>
+    </div>
+    <div class="statistic">
+        <div class="boxcenter">
+            <h1>Einami metai <br>{{amount.thisYear}}</h1>
+        </div>
+    </div>
+    <div class="statistic">
+        <div class="boxcenter">
+            <h1>Ketvirtis <br>{{amount.thisQuarter}}</h1>
+        </div>
+    </div>
+    <div class="statistic">
+        <div class="boxcenter">
+            <h1>Pusmetis <br>{{amount.halfYear}}</h1>
+        </div>
+    </div>
+</div>
+
+
 
 {% endblock %}
-
